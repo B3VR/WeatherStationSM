fd8abd11dbef0421b1d77a34db722102e1542c9c
B3VR <73071619+B3VR@users.noreply.github.com>
2021-08-23 14:30:00 +0200

    Add files via upload

--- Termometr_SM/main.c
+++ Termometr_SM/main.c
@@ -2,9 +2,13 @@
  * Termometr_SM.c
  *
  * Created: 02.04.2021 17:01:47
- * Author : Krystian Miszta, Julia Leszczyñska
+ * Author : Krystian Miszta, Julia Leszczynska
  */ 
 #define  F_CPU 16000000
+#define SDA_PORT PORTC
+#define SDA_PIN 4
+#define SCL_PORT PORTC
+#define SCL_PIN 5
 
 #include <stdlib.h>
 #include <stdio.h>
@@ -12,8 +16,7 @@
 #include <util/delay.h>
 #include "hd44780.h"
 #include "dhtxx.h"
-
-
+#include "SoftI2CMaster.h"
 
 
 int main(void)
@@ -23,6 +26,7 @@ int main(void)
 	PORTD |= (1<<3);
 	
 	lcd_init();
+	i2c_init();
     while(1){		
 		
 		if (PIND & (1<<3))
@@ -49,6 +53,40 @@ void bodyTemperature()
 	_delay_ms(1000);
 	lcd_puts("1");
 	_delay_ms(1000);	
+	lcd_clrscr();
+	float bodyTemperature=getBodyTemperature();
+	lcd_puts(bodyTemperature);
+	lcd_puts(" C");
+	_delay_ms(1000);
+}
+
+float getBodyTemperature()
+}
+	int dev = 0x5A<<1;
+    int data_low = 0;
+    int data_high = 0;
+    int pec = 0;
+    
+    i2c_start(dev+I2C_WRITE);
+    i2c_write(0x07);
+    // read
+    i2c_rep_start(dev+I2C_READ);
+    data_low = i2c_read(false); //Read 1 byte and then send ack
+    data_high = i2c_read(false); //Read 1 byte and then send ack
+    pec = i2c_read(true);
+    i2c_stop();
+    
+    //This converts high and low bytes together and processes temperature, MSB is a error bit and is ignored for temps
+    double tempFactor = 0.02; // 0.02 degrees per LSB (measurement resolution of the MLX90614)
+    double tempData = 0x0000; // zero out the data
+    int frac; // data past the decimal point
+    
+    // This masks off the error bit of the high byte, then moves it left 8 bits and adds the low byte.
+    tempData = (double)(((data_high & 0x007F) << 8) + data_low);
+    tempData = (tempData * tempFactor)-0.01;
+    
+    float celcius = tempData - 273.15;
+	return celcius;
 }
 
 void weatherStation()
